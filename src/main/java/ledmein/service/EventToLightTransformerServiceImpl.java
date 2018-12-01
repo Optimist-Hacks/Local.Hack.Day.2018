b5918ecaedea4193a314d2457f8f6806d349e504
Aleksandr Yurkovskiy <sanekyy@gmail.com>
2018-12-01 20:31:57 +0300

    add color logs

--- src/main/java/ledmein/service/EventToLightTransformerServiceImpl.java
+++ src/main/java/ledmein/service/EventToLightTransformerServiceImpl.java
@@ -2,6 +2,8 @@ package ledmein.service;
 
 import ledmein.model.Event;
 import ledmein.repository.authorToColorRepository.DefaultAuthorsColorsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import static ledmein.util.Lights.*;
 @Service
 public class EventToLightTransformerServiceImpl implements EventToLightTransformerService {
 
+    private static Logger logger = LoggerFactory.getLogger(EventToLightTransformerServiceImpl.class);
+
     @Autowired
     DefaultAuthorsColorsRepository authorsColorsRepo;
 
@@ -19,20 +23,29 @@ public class EventToLightTransformerServiceImpl implements EventToLightTransform
     public Color transformToRGB(Event event) {
         switch (event.getEventType()) {
             case COMMIT:
+                logger.info("commit PERSONAL color");
                 return getPersonalColor(event);
             case PULL_REQUEST:
+                logger.info("pull_request BLACK color");
                 return PULL_REQUEST_COLOR;
             case PUSH:
+                logger.info("push CYAN color");
                 return PUSH_COLOR;
             case FORK:
+                logger.info("fork PINK color");
                 return FORK_COLOR;
             case BUILD_STARTED:
+                logger.info("started YELLOW color");
                 return BUILD_STARTED_COLOR;
             case BUILD_FAILED:
+                logger.info("failed RED color");
                 return BUILD_FAILED_COLOR;
             case BUILD_SUCCESS:
+                logger.info("success GREEN color");
                 return BUILD_SUCCESS_COLOR;
         }
+
+        logger.info("default WHITE color");
         return DEFAULT_COLOR;
     }
 
