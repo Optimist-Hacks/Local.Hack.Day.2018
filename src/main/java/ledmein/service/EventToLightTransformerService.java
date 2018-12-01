82b4440b5dc5f0f6c143b3c1d3df2caaff740ecd
German Abramov <german_abramov@epam.com>
2018-12-01 11:02:38 +0300

    add rgb model

--- src/main/java/ledmein/service/EventToLightTransformerService.java
+++ src/main/java/ledmein/service/EventToLightTransformerService.java
@@ -5,10 +5,19 @@ import ledmein.model.RGB;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public interface EventToLightTransformerService {
-    default List<RGB> transformToByteArray(List<Event> events) {
+    default List<RGB> transformToRGB(List<Event> events) {
+        return events
+                .stream()
+                .map(EventToLightTransformerService::transformOne)
+                .collect(Collectors.toList());
+    }
+
+    static RGB transformOne(Event event){
+
         return null;
     }
 }
