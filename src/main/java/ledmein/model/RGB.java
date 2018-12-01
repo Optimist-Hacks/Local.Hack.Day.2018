3ab412b4980ba91715de47d6829aad3d920c7f57
German Abramov <german_abramov@epam.com>
2018-12-01 10:57:37 +0300

    add rgb model

---
+++ src/main/java/ledmein/model/RGB.java
@@ -0,0 +1,8 @@
+package ledmein.model;
+
+import lombok.Data;
+
+@Data
+public class RGB {
+    int[] values = new int[3];
+}
--- src/main/java/ledmein/service/EventToLightTransformerService.java
+++ src/main/java/ledmein/service/EventToLightTransformerService.java
@@ -1,7 +1,14 @@
 package ledmein.service;
 
+import ledmein.model.Event;
+import ledmein.model.RGB;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class EventToLightTransformerService {
+public interface EventToLightTransformerService {
+    default List<RGB> transformToByteArray(List<Event> events) {
+        return null;
+    }
 }
