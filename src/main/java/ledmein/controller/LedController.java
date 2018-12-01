bfcfbc0c6be8246297492abb0b70906ef97add09
German Abramov <german_abramov@epam.com>
2018-12-01 15:34:29 +0300

    glue services together

--- src/main/java/ledmein/controller/LedController.java
+++ src/main/java/ledmein/controller/LedController.java
@@ -1,10 +1,9 @@
 package ledmein.controller;
 
 import io.reactivex.disposables.Disposable;
-import ledmein.model.Event;
 import ledmein.repository.eventRepositiry.DefaultEventRepository;
 import ledmein.repository.eventRepositiry.EventRepository;
-import ledmein.service.EventToLightTransformerService;
+import ledmein.service.ArduinoService;
 import ledmein.service.EventToLightTransformerServiceImpl;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -12,12 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.awt.*;
-import java.util.List;
 
 @Controller
 public class LedController {
@@ -25,14 +19,16 @@ public class LedController {
     private static Logger logger = LoggerFactory.getLogger(LedController.class);
     private EventRepository repository;
     private EventToLightTransformerServiceImpl service;
+    private ArduinoService arduinoService;
 
     @Nullable
     private Disposable onNextEventDisposable;
 
     @Autowired
-    public LedController(DefaultEventRepository repository, EventToLightTransformerServiceImpl service) {
+    public LedController(DefaultEventRepository repository, EventToLightTransformerServiceImpl service, ArduinoService arduinoService) {
         this.repository = repository;
         this.service = service;
+        this.arduinoService = arduinoService;
     }
 
     @GetMapping("/rgb")
@@ -50,6 +46,7 @@ public class LedController {
 
         onNextEventDisposable = repository.onNextEvent(login, repo)
                 .map(service::transformToRGB)
+                .doOnNext(arduinoService::writeColor)
                 .subscribe();
     }
 
